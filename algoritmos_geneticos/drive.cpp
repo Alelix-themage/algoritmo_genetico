dd77c071ae82ed55f1d831e9640e653f4ea12eaf
Alelix-themage <alessandrojunior864@gmail.com>
2024-03-23 16:31:12 -0300

    alteracao feita

--- algoritmos_geneticos/drive.cpp
+++ algoritmos_geneticos/drive.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 //Utilizamos a função bitwise porque ela trabalha bit a bit, assim fica mais fácil de mudar o "bit" genético
 
+//struct com o individuo e o array de individuo
+struct Individuos{
+    int individuo = 0;
+    int individuos[];
+};
 
 void retornaBin(int a){
     //Converte um valor decimal em binario
@@ -13,41 +18,57 @@ void retornaBin(int a){
    
 } 
 
-//Definir um intervalo (-2000, 2000) para que achemos uma boa solução 
-
-
-int main(){
+void populacaoInicial(int tamanho){
+    Individuos ind;
     srand(time(0));
-    int individuo1 = 0;
-    int individuo2 = 0;
-    char op;
+    char opc;
+    int max = 2000;
+    int min = -2000;
+   
+    cout << "Deseja gerar uma populacao aleatoria? (s/n)" << endl;
+    cin >> opc;
+    for(int i = 0; i < tamanho; i++){
+      int individuo_aleatorio = min +rand() % (max - min +1);
     
-    cout<<"Deseja iniciar o programa? (s/n)" << endl;
-    
-    cin >> op;
-    if(op == 's'){
-        char op;
-        cout << "Deseja criar dois individuos aleatorios? (s/n)" << endl;
-        cin >> op;
-        if(op == 's'){
-            //cria dois individuos geneticamente diferentes
-            cout << "\nEste e o indivduo 2: ";
-            individuo1 = rand();
-            cout << individuo1 << endl;
-            retornaBin(individuo1);
-
-            cout << "\nEste e o indivduo 2: ";
-            individuo2 = rand();
-            cout << individuo2 << endl;
-            retornaBin(individuo2);
-                
-                
-
+     // criar dois individuos geneticamente diferentes 
+        if(opc == 's'){
+            cout << "\nEstes e o individuos : " << endl;
+            ind.individuos[i] = individuo_aleatorio;
+            cout<< ind.individuos[i] << endl;
+            retornaBin(ind.individuos[i]);
+            cout <<"\n" << endl;
         }
     }
-    else if(op == 'n'){
-        cout << "O programa sera encerrado." << endl;
+}
+
+ 
+  
+int main(){
+    srand(time(0));
+    char op;
+   int tamanho_da_populacao = 0;
+    
+    cout<<"Deseja iniciar o programa? (s/n)" << endl;
+    cin >> op;
+    if(op =='s'){
+        cout << "Escolha o tamanho da sua populacao: {10, 100, 1000}" << endl;
+        cin >> tamanho_da_populacao;
+        if(tamanho_da_populacao == 10 || tamanho_da_populacao == 100 || tamanho_da_populacao == 1000){   
+            populacaoInicial(tamanho_da_populacao);
+        }
+        else {
+            cout<<"Nao sao permitidas populacoes que nao tenham tamanho de 10, 100 ou 1000!" << endl;
+            return 0;
+        }
+    }
+
+    else{
+        cout<<"O programa sera encerrado!" << endl;
         return 0;
     }
 
+   
+    
+    
 }
+
