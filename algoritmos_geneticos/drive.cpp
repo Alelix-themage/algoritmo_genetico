06b3c3a3bb829aefbf2e170a236553f1dd20c636
Alelix-themage <alessandrojunior864@gmail.com>
2024-03-23 14:29:45 -0300

    ok

--- algoritmos_geneticos/drive.cpp
+++ algoritmos_geneticos/drive.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
 using namespace std;
+//Utilizamos a função bitwise porque ela trabalha bit a bit, assim fica mais fácil de mudar o "bit" genético
+
+
+void retornaBin(int a){
+    //Converte um valor decimal em binario
+    int bit = 0;
+    for(int i = 31; i >= 0; i--){ //vai passar bit a bit o numero que passamos com sentido a direita, sempre analisando se deveria ser 1 ou 0
+        bit = (a >> i) & 1; 
+        cout << bit;
+    }
+   
+} 
+
 
 int main(){
-    cout << "Hello World";
-    //seila
-    int soma = 0;
-    return 0;
+    srand(time(0));
+    int individuo1 = 0;
+    int individuo2 = 0;
+    char op;
+    
+    cout<<"Deseja iniciar o programa? (s/n)" << endl;
+    
+    cin >> op;
+    if(op == 's'){
+        char op;
+        cout << "Deseja criar dois individuos aleatorios? (s/n)" << endl;
+        cin >> op;
+        if(op == 's'){
+            cout << "\nEste e o indivduo 1: ";
+            individuo1 = rand();
+            cout << individuo1 << endl;
+            retornaBin(individuo1);
+
+            cout << "\nEste e o indivduo 2: ";
+            individuo2 = rand();
+            cout << individuo2 << endl;
+            retornaBin(individuo2);
+                
+                
+
+        }
+    }
+    else if(op == 'n'){
+        cout << "O programa sera encerrado." << endl;
+        return 0;
+    }
+
 }
